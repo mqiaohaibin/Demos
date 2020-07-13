2f04a5a9d2c654dbe0d0159d3b09954f28553613
qiaohaibin <qiaohaibin@xiaomi.com>
2020-07-14 07:32:34 +0800

    add system scroll

--- Scroll/app/src/main/java/cn/paper/demo/scroll/MainActivity.kt
+++ Scroll/app/src/main/java/cn/paper/demo/scroll/MainActivity.kt
@@ -1,12 +1,29 @@
 package cn.paper.demo.scroll
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnSystemScroll: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSystemScroll = findViewById(R.id.btnSystemScroll)
+        btnSystemScroll.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.btnSystemScroll -> {
+                val intent = Intent(this, SystemScrollActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ Scroll/app/src/main/java/cn/paper/demo/scroll/SystemScrollActivity.kt
@@ -0,0 +1,12 @@
+package cn.paper.demo.scroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SystemScrollActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_system_scroll)
+    }
+}
